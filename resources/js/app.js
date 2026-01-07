87a157fb2aad9e58bfb916ecac44abcf159848ea
amralihasan <amr@zumra.sa>
2026-01-07 22:34:44 +0200

    Add Vue.js integration with Pusher support and HomeComponent

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+
+import Vue from 'vue';
+import HomeComponent from './components/HomeComponent.vue';
+
+Vue.component('home-component', HomeComponent);
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        test: 'Vue is working!'
+    },
+    mounted() {
+        console.log('Vue app mounted!', this.$el);
+        console.log('Test data:', this.test);
+    }
+});
